b90cf208785b74168b6aa00e4724a439a251106a
Cole Gerdemann <corvettecole@gmail.com>
2022-09-08 13:35:44 -0400

    status bar improvements, keep constants in relevant class

--- lib/widgets/status_bar.dart
+++ lib/widgets/status_bar.dart
@@ -3,6 +3,16 @@ import 'package:provider/provider.dart';
 import 'package:uksc_dashboard/websocket.dart';
 import 'package:uksc_dashboard/constants.dart';
 
+/// The preferred height of the status bar
+const statusBarHeight = 35.0;
+
+/// The elevation of the statusBar
+// TODO: would prefer not to use this at all
+const statusBarElevation = 1.0;
+
+/// The spacing between provided children to be placed in the status bar
+const statusBarChildrenSpacing = 10.0;
+
 /// Represents a status bar, displaying essential information such as connection status, time, etc.
 ///
 /// This widget is meant to be used as an [AppBar] for the [Scaffold] of a dashboard. Can be passed
@@ -24,15 +34,18 @@ class StatusBar extends StatelessWidget implements PreferredSizeWidget {
         surfaceTintColor: appBarTheme.surfaceTintColor,
         shape: appBarTheme.shape,
         child: Row(
-          mainAxisAlignment: MainAxisAlignment.start,
           children: [
             const ConnectionStatus(),
+            Expanded(
+                child: Align(
+              alignment: Alignment.center,
+              child: Wrap(spacing: statusBarChildrenSpacing, children: [...?children]),
+            )),
             const Expanded(
                 child: Align(
               alignment: Alignment.centerRight,
               child: Clock(),
             )),
-            ...?children,
           ],
         ));
   }
@@ -50,10 +63,10 @@ class Clock extends StatelessWidget {
       stream: Stream.periodic(const Duration(seconds: 1)),
       builder: (context, snapshot) {
         return SizedBox(
-            height: statusBarHeight,
+            height: double.infinity,
             width: 70,
-            child: Container(
-                margin: const EdgeInsets.symmetric(vertical: 7, horizontal: 4),
+            child: Align(
+                alignment: Alignment.center,
                 child: Text(
                     '${DateTime.now().hour.toString().padLeft(2, '0')}:${DateTime.now().minute.toString().padLeft(2, '0')}:${DateTime.now().second.toString().padLeft(2, '0')}',
                     style: Theme.of(context).textTheme.titleMedium,
@@ -70,45 +83,45 @@ class ConnectionStatus extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Consumer<WebSocketStatus>(builder: (context, webSocketStatus, child) {
-      Widget statusWidget;
+      Widget statusIcon;
+      Widget statusText;
       switch (webSocketStatus.status) {
         case Status.connected:
-          statusWidget = Row(children: [
-            Container(
-                margin: const EdgeInsets.symmetric(horizontal: 2),
-                child: const Icon(Icons.sync, size: 24, color: Colors.green)),
-            Container(
-                margin: const EdgeInsets.symmetric(horizontal: 2),
-                child: Text('(${webSocketStatus.averageLatencyMs.toStringAsFixed(1)}ms)',
-                    style: Theme.of(context).textTheme.titleMedium, textAlign: TextAlign.center))
-          ]);
+          statusIcon = const Icon(Icons.sync, size: 24, color: Colors.green);
+          statusText = Text('${webSocketStatus.averageLatencyMs.toStringAsFixed(1)}ms',
+              style: Theme.of(context).textTheme.titleMedium, textAlign: TextAlign.center);
+
           break;
         case Status.connecting:
-          statusWidget = Row(children: [
-            Container(
-                margin: const EdgeInsets.symmetric(horizontal: 2),
-                child: const Center(
-                    child: Padding(
-                        padding: EdgeInsets.fromLTRB(4.0, 0.0, 10.0, 0.0),
-                        child: SizedBox(
-                            height: 16,
-                            width: 16,
-                            child: CircularProgressIndicator(
-                              value: null,
-                              strokeWidth: 3,
-                            ))))),
-            Container(
-                margin: const EdgeInsets.symmetric(horizontal: 2),
-                child:
-                    Text('Connecting...', style: Theme.of(context).textTheme.titleMedium, textAlign: TextAlign.center)),
-          ]);
+          // padding object here is a workaround for https://github.com/flutter/flutter/issues/3282
+          statusIcon = const Padding(
+              padding: EdgeInsets.symmetric(horizontal: 4.0),
+              child: SizedBox(
+                  height: 16,
+                  width: 16,
+                  child: CircularProgressIndicator(
+                    value: null,
+                    strokeWidth: 3,
+                  )));
+          statusText =
+              Text('Connecting...', style: Theme.of(context).textTheme.titleMedium, textAlign: TextAlign.center);
           break;
         case Status.disconnected:
-          statusWidget = const Icon(Icons.sync_problem, size: 24, color: Colors.orange);
+          statusIcon = const Icon(Icons.sync_problem, size: 24, color: Colors.orange);
+          statusText =
+              Text('Disconnected', style: Theme.of(context).textTheme.titleMedium, textAlign: TextAlign.center);
           break;
       }
 
-      return SizedBox(height: statusBarHeight, width: null, child: statusWidget);
+      return Row(
+        children: [
+          statusIcon,
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 2),
+            child: statusText,
+          )
+        ],
+      );
     });
   }
 }
