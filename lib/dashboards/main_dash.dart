ca90fc70dcace286934f2718d2d23c8af9f714e0
gmabboud <gmab222@uky.edu>
2024-05-08 15:57:48 -0400

    Fixed throttle sizing added timing stuff, first official version of my dash

--- lib/dashboards/main_dash.dart
+++ lib/dashboards/main_dash.dart
@@ -1,11 +1,14 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:uksc_dashboard/models/speed.dart';
 
 import 'package:uksc_dashboard/widgets/speeds_display.dart';
 import 'package:uksc_dashboard/widgets/status_bar.dart';
 import 'package:uksc_dashboard/widgets/throttle.dart';
 import 'package:uksc_dashboard/widgets/indicator_lights.dart';
 import 'package:uksc_dashboard/widgets/battery_indicators.dart'; // Import the BatteryDisplay widget
-
+import 'package:uksc_dashboard/widgets/time_indicators.dart';
 class MainDashboard extends StatelessWidget {
   const MainDashboard({Key? key}) : super(key: key);
 
@@ -23,58 +26,39 @@ class MainDashboard extends StatelessWidget {
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 Container(
-                  padding: const EdgeInsets.all(3),
-                  child: Stack(
-                    alignment: Alignment.center,
-                    children: [
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: [
-                          Container(
-                            decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(1),
-                              color: Colors.white,
-                            ),
-                            child: const Padding(
-                              padding: EdgeInsets.all(1),
-                              child: BatteryDisplay(), // Add BatteryDisplay widget here
-                            ),
-                          ),
-                          const SizedBox(height: 10), // Adjust as needed for spacing
-                        ],
-                      ),
-                    ],
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(2),
+                    border: Border.all(color: Colors.white, width: 2),
                   ),
+                  padding: const EdgeInsets.all(5),
+                  child: const BatteryDisplay(),
                 ),
+                const SizedBox(width: 10),
                 Container(
-                  padding: const EdgeInsets.all(3),
-                  child: Stack(
-                    alignment: Alignment.center,
-                    children: [
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: [
-                          Container(
-                            decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(5),
-                              color: Colors.white,
-                            ),
-                            child: const SpeedDisplay(),
-                          ),
-                          const SizedBox(height: 10), // Adjust as needed for spacing
-                        ],
-                      ),
-                    ],
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(2),
+                    border: Border.all(color: Colors.white, width: 2),
                   ),
+                  padding: const EdgeInsets.all(5),
+                  child: const SpeedDisplay(),
                 ),
                 const SizedBox(width: 10), // Adjust as needed for spacing
                 Container(
                   decoration: BoxDecoration(
                     borderRadius: BorderRadius.circular(2),
-                    border: Border.all(color: Colors.white),
+                    border: Border.all(color: Colors.white, width: 2),
                   ),
-                  padding: const EdgeInsets.all(10),
+                  padding: const EdgeInsets.all(5),
                   child: const Throttle(),
+                ),
+                const SizedBox(width: 10),
+                Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(2),
+                    border: Border.all(color: Colors.white, width: 2),
+                  ),
+                  padding: const EdgeInsets.all(2),
+                  child: const TimeReadout(),
                 ),
               ],
             ),
---
+++ lib/models/timing.dart
@@ -0,0 +1,19 @@
+import 'package:provider/provider.dart';
+import 'package:uksc_dashboard/models/base_model.dart';
+
+class Timing extends BaseModel {
+
+  Timing()
+      : super({
+          'Vehicle.CurrentLocation.LapTime' : '0',
+          'Vehicle.CurrentLocation.Timestamp' : '0',
+        });
+
+  double get lapTime => double.parse(data['Vehicle.CurrentLocation.LapTime']);
+  double get unixTime => double.parse(data['Vehicle.CurrentLocation.Timestamp']);
+
+
+ @override
+  ChangeNotifierProvider<Timing> get provider =>
+      ChangeNotifierProvider.value(value: this);
+}
