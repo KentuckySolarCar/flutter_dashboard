39353e8d756d2bb5cb174a0a2c995e4aa9e317d4
gmabboud <gmab222@uky.edu>
2025-03-10 18:31:35 -0400

    Added linux build code that enables program to always start in full screen

--- linux/my_application.cc
+++ linux/my_application.cc
@@ -48,6 +48,7 @@ static void my_application_activate(GApplication* application) {
   }
 
   gtk_window_set_default_size(window, 800, 480);
+  gtk_window_fullscreen(GTK_WINDOW(window)); // This works to have the app start in full screen!
   gtk_widget_show(GTK_WIDGET(window));
 
   g_autoptr(FlDartProject) project = fl_dart_project_new();
